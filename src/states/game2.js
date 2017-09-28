496990e621251011c605c0ca35d8feb99cb6e8d2
Jan Barandun <jb@clickwerk.ch>
2017-09-28 22:55:57 +0200

    added simple volley shot

--- src/states/game2.js
+++ src/states/game2.js
@@ -11,23 +11,42 @@ export default class Game2 extends Phaser.State {
         this.game.physics.arcade.gravity.y = 400;
 
         this.game.load.image('net', 'assets/img/net-1.png');
+        this.game.load.image('ball', 'assets/img/ball.png');
         
 
     }
 
 
     create() {
-        console.log(this.game.width)
-        this.net = this.game.add.sprite(910, 450, 'net');
         
-        console.log(this.net.width)
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.net = this.game.add.sprite(910, 450, 'net');
+        this.ball = this.game.add.sprite(300, 600, 'ball');
+
+        this.game.physics.enable([this.ball,this.net]);
+
+        this.net.body.immovable = true;
+
+        this.ball.body.bounce.set(0.8);
+        this.ball.body.collideWorldBounds = true;
+
+        this.ball.inputEnabled = true;
+
+        this.ball.events.onInputUp.add(this.startBall, this);
 
     }
 
     update () {
-      
         
+        this.game.physics.arcade.collide(this.net, this.ball);
+
       
-      }
+    }
+
+    startBall () {
+        this.ball.body.velocity.setTo(500,-600)
+        this.ball.body.gravity.y = 400;
+    }
 
 }
