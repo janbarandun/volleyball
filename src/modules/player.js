37c8fd880c33811dfad439763fb0df6e6132132e
Jan Barandun <jb@clickwerk.ch>
2017-10-15 22:57:14 +0200

    in change to p2 physics, not really working but first functions working

--- src/modules/player.js
+++ src/modules/player.js
@@ -14,26 +14,38 @@ export default class Player extends Phaser.Sprite {
     update() {
 
         this.body.velocity.x = 0;
-
-        if (this.cursors.left.isDown)
-        {
-            this.body.velocity.x -= 400;
+        
+        let cur = this.cursors;
+        if (cur.left.isDown) {
+            this.body.velocity.x = -350;
+            //player.animations.play('left');
         }
-        else if (this.cursors.right.isDown)
-        {
-            this.body.velocity.x += 400;
+        else if (cur.right.isDown) {
+            this.body.velocity.x = 350;
+            //player.animations.play('right');
         }
-    
-        if (this.cursors.up.isDown) {
+        else {
+            //player.animations.stop();
+            //this.player.frame = 4;
+        }
+        //  Allow the player to jump if touching the ground.
+        if (cur.up.isDown && this.touchingDown(this)) {
+            this.body.velocity.y = -1200;
+        }
+    }
 
-            if (this.body.onFloor()){
-                this.body.velocity.y = -800;
+    touchingDown(someone) {
+        var yAxis = p2.vec2.fromValues(0, 1);
+        var result = false;
+        for (var i = 0; i < this.game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+            var c = this.game.physics.p2.world.narrowphase.contactEquations[i];  // cycles through all the contactEquations until it finds our "someone"
+            if (c.bodyA === someone.body.data || c.bodyB === someone.body.data) {
+                var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
+                if (c.bodyA === someone.body.data) d *= -1;
+                if (d > 0.5) result = true;
             }
         }
-        else if (this.cursors.down.isDown)
-        {
-           // this.y += 4;
-        }
+        return result;
     }
 
 }
--- src/states/main.js
+++ src/states/main.js
@@ -2,10 +2,11 @@
 import {default as Player} from '../modules/player.js';
 import {default as Enemy} from '../modules/enemy.js';
 
-export default class Game2 extends Phaser.State {
+export default class Main extends Phaser.State {
     
     preload() {
 
+        this.game.load.physics('physicsData', '/assets/sprites.json');
         
 
     }
@@ -19,29 +20,39 @@ export default class Game2 extends Phaser.State {
         this.player2 = new Enemy(this.game,1600,1000,this.playerGroup);
 
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
 
         //  Set the world (global) gravity
-        this.game.physics.arcade.gravity.y = 400;
-        
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.p2.gravity.y = 2000;
 
-        this.net = this.game.add.sprite(910, 450, 'net');
+        this.net = this.game.add.sprite(910, 700, 'net');
         this.ball = this.game.add.sprite(300, 800, 'ball');
 
 
 
-        this.game.physics.enable([this.ball,this.net,this.player1,this.player2]);
+        this.game.physics.p2.enable([this.ball,this.net,this.player1,this.player2],true);
 
-        this.net.body.immovable = true;
-        this.player1.body.immovable = true;
-        this.player2.body.immovable = true;
+        //this.net.body.immovable = true;
+
+        this.player1.body.clearShapes();
+        this.player1.body.loadPolygon('physicsData', 'player');
+
+        this.player2.body.clearShapes();
+        this.player2.body.loadPolygon('physicsData', 'player');
+
+        this.ball.body.clearShapes();
+        this.ball.body.loadPolygon('physicsData', 'ball');
+
+        this.net.body.static = true;
+        //this.player2.body.immovable = true;
 
         this.player1.body.gravity.y = 1200;
         this.player1.body.collideWorldBounds = true;
+        this.player1.fixedRotation = true;
 
-
-        this.ball.body.bounce.set(0.8);
+        var playerMaterial = this.game.physics.p2.createMaterial('playerMaterial', this.player1.body);
+        var worldMaterial = this.game.physics.p2.createMaterial('worldMaterial');
+        //this.ball.body.bounce.set(0.8);
         // ???? how to set a different bounce when colliding with wall or net ????
 
 
@@ -55,8 +66,8 @@ export default class Game2 extends Phaser.State {
 
     update () {
         
-        this.game.physics.arcade.collide(this.net, this.ball);
-        this.game.physics.arcade.collide(this.playerGroup, this.ball,this.hitBall); 
+        //this.game.physics.p2.collide(this.net, this.ball);
+        //this.game.physics.p2.collide(this.playerGroup, this.ball,this.hitBall); 
 
         // collide net and player
     
